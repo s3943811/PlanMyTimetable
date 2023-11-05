72fdb1ea9bd3bd2c7e084bba3f10ddb10aef12a3
s3943811 <s3943811@student.rmit.edu.au>
2023-11-05 17:05:02 +1100

    Update calendar layout and time slots

--- src/app/(Planner)/Calendar.tsx
+++ src/app/(Planner)/Calendar.tsx
@@ -32,13 +32,13 @@ function Day({day, children}: {day: string, children: React.ReactNode}) {
 function Timeline() {
     const times: Array<string> = getTimes();
     return (
-        <div className="flex flex-col justify-between h-full w-16 border-r"> 
+        <div className="flex flex-col h-full w-16 border-r"> 
             <div className="pb-2 sticky top-0 w-full bg-stone-50 border-b">
                 <h3 className="text-lg font-semibold invisible">Times</h3>
             </div>
-            <div className="divide-y">
+            <div className="divide-y border-b h-full">
                 {times.map((time: string) => (
-                    <p className={`text-xs ${time.includes(':15') || time.includes(':45') ? 'invisible' : ''}`} key={time}>{time}</p>
+                    <p className={`text-xs h-7 ${time.includes(':30') ? 'invisible' : ''}`} key={time}>{time}</p>
                 ))}
             </div>
         </div>
@@ -47,16 +47,16 @@ function Timeline() {
 
 function DayGrid() {
     const timeSlots = [];
-    for (let i = 0; i < 96; i++) {
+    for (let i = 0; i < 48; i++) {
         timeSlots.push(<TimeSlot key={i} />);
     }
     return (
-        <div className="grid grid-rows-96 divide-y w-full border-r">{timeSlots}</div>
+        <div className="grid grid-rows-48 divide-y w-full border-r border-b">{timeSlots}</div>
     )
 }
 
 function TimeSlot() {
-    return <div className="h-[17px] hover:bg-stone-100"></div>
+    return <div className="h-7 hover:bg-stone-100"></div>
 }
 
 export default Calendar;
