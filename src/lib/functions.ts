40ab9eb6057c9ca0517dc7d1bf8343d868470250
s3943811 <s3943811@student.rmit.edu.au>
2023-11-05 16:52:48 +1100

    Add function to get times in 15 minute intervals

---
+++ src/lib/functions.ts
@@ -0,0 +1,12 @@
+function getTimes(): Array<string> {
+    let times = [];
+    let time = new Date();
+    time.setHours(0,0,0,0);
+    while(time.getDate() === new Date().getDate()) {
+        times.push(new Date(time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true}));
+        time.setMinutes(time.getMinutes() + 15);
+    }
+    return times;
+}
+
+export {getTimes}
