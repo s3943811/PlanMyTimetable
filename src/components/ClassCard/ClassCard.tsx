51bded34851573dd8a1b4bfd98bac4430b0226e9
s3943811 <s3943811@student.rmit.edu.au>
2023-11-06 17:41:38 +1100

    Fix border colour issue for ClassCard

--- src/components/ClassCard/ClassCard.tsx
+++ src/components/ClassCard/ClassCard.tsx
@@ -8,6 +8,7 @@ export interface ClassCardProps {
   colour: string;
 }
 
+// https://tailwindcss.com/docs/content-configuration#dynamic-class-names
 function ClassCard({ title, courseCode, type, colour }: ClassCardProps) {
   var cardColour;
   switch (type) {
@@ -22,9 +23,7 @@ function ClassCard({ title, courseCode, type, colour }: ClassCardProps) {
       break;
   }
   return (
-    <div
-      className={`flex w-72 flex-col gap-1 border-r-8 p-5 py-2.5 ${colour} rounded-md shadow-sm hover:bg-stone-100`}
-    >
+    <div className={`flex w-72 flex-col gap-1 border-r-8 p-5 py-2.5 rounded-md shadow-sm hover:bg-stone-100 ${colour}`}>
       <h1 className="line-clamp-1 text-lg font-medium">{title}</h1>
       <p className=" text-xs font-light text-gray-400">{courseCode}</p>
       <Badge className={cardColour + " px-4 text-sm"}>{CourseType[type]}</Badge>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.{tsx,ts}"],
   theme: {
     extend: {
       gridTemplateRows: {
