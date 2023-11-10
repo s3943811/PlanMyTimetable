b709386ead974134f42e546b79525385d7728d58
s3943811 <s3943811@student.rmit.edu.au>
2023-11-11 01:12:58 +1100

    Add new functions and update Event component styles

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -1,5 +1,9 @@
 import { CourseType, Time, ColourPalette } from "~/lib/definitions";
-import { addMinutesToTimeString } from "~/lib/functions";
+import {
+  addMinutesToTimeString,
+  getDayEnum,
+  getRowIndex,
+} from "~/lib/functions";
 interface EventProps {
   title: string;
   type: CourseType;
@@ -9,16 +13,27 @@ interface EventProps {
 export default function Event({ title, type, colour, time }: EventProps) {
   const endTime = addMinutesToTimeString(time.start, time.duration);
   const colourVariants = {
-    0: "bg-purple-400 text-purple-800 shadow-purple-100 border-l-[7px] border-l-purple-500",
-    1: "bg-yellow-400 text-yellow-800 shadow-yellow-100 border-l-[7px] border-l-yellow-500",
-    2: "bg-orange-400 text-orange-800 shadow-orange-100 border-l-[7px] border-l-orange-500",
-    3: "bg-red-400 text-red-800 shadow-red-100 border-l-[7px] border-l-red-500",
+    0: "bg-purple-400/40 text-purple-800 shadow-purple-100 border-l-[7px] border-l-purple-500",
+    1: "bg-yellow-400/40 text-yellow-800 shadow-yellow-100 border-l-[7px] border-l-yellow-500",
+    2: "bg-orange-400/40 text-orange-800 shadow-orange-100 border-l-[7px] border-l-orange-500",
+    3: "bg-red-400/40 text-red-800 shadow-red-100 border-l-[7px] border-l-red-500",
+  };
+  const colStart = `col-start-[${getDayEnum(time.day)! + 2}]`;
+  const rowStart = `row-start-[${getRowIndex(time.start)}]`;
+  const rowSpan: number = time.duration / 30;
+  const rowSpans: { [key: number]: string } = {
+    1: "row-span-1",
+    2: "row-span-2",
+    3: "row-span-3",
+    4: "row-span-4",
+    5: "row-span-5",
+    6: "row-span-6",
   };
   return (
     <div
-      className={`flex flex-col ${colourVariants[colour]} rounded bg-opacity-60 px-3 py-2`}
+      className={`${colStart} ${rowStart} ${rowSpans[rowSpan]} m-0.5 flex flex-col overflow-hidden ${colourVariants[colour]} rounded px-3 py-2`}
     >
-      <h1 className="line-clamp-1 font-medium">{title}</h1>
+      <h1 className="font-medium">{title}</h1>
       <p className="text-sm font-normal">
         {time.start} - {endTime}
       </p>
