07c1245c9381434a9d3c44df096555734eb8c774
s3943811 <s3943811@student.rmit.edu.au>
2023-11-11 16:28:36 +1100

    Remove unused components and refactor PreviewEvent

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,6 @@ export { default as Navbar } from "./Navbar/navbar";
 export { default as Badge } from "./Badge/Badge";
 export { default as ClassCard } from "./ClassCard/ClassCard";
 export { default as ClassCardClient } from "./ClassCard/ClassCardClient";
-export { default as DayGrid } from "./Calendar/DayGrid";
 export { default as CalendarHeader } from "./Calendar/CalendarHeader";
 export { default as TimeSlot } from "./Calendar/TimeSlot";
 export { default as Button } from "./Button/Button";
