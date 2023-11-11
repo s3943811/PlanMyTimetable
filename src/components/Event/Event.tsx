ac905fc7b1fdc2e462753cb853d7d5116d92abef
s3943811 <s3943811@student.rmit.edu.au>
2023-11-11 15:36:04 +1100

    Add colStart and rowStart to Event component

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -1,4 +1,10 @@
-import { CourseType, Time, ColourPalette } from "~/lib/definitions";
+import {
+  CourseType,
+  Time,
+  ColourPalette,
+  colStart,
+  rowStart,
+} from "~/lib/definitions";
 import {
   addMinutesToTimeString,
   getDayEnum,
@@ -18,10 +24,8 @@ export default function Event({ title, type, colour, time }: EventProps) {
     2: "bg-orange-400/40 text-orange-800 shadow-orange-100 border-l-[7px] border-l-orange-500",
     3: "bg-red-400/40 text-red-800 shadow-red-100 border-l-[7px] border-l-red-500",
   };
-  const colStart = `col-start-[${getDayEnum(time.day)! + 2}]`;
-  const rowStart = `row-start-[${getRowIndex(time.start)}]`;
   const rowSpan: number = time.duration / 30;
-  const rowSpans: { [key: number]: string } = {
+  const rowSpans: Record<number, string> = {
     1: "row-span-1",
     2: "row-span-2",
     3: "row-span-3",
@@ -31,7 +35,11 @@ export default function Event({ title, type, colour, time }: EventProps) {
   };
   return (
     <div
-      className={`${colStart} ${rowStart} ${rowSpans[rowSpan]} m-0.5 flex flex-col overflow-hidden ${colourVariants[colour]} rounded px-3 py-2`}
+      className={`${colStart[getDayEnum(time.day)! + 2]} ${
+        rowStart[getRowIndex(time.start)]
+      } ${rowSpans[rowSpan]} m-0.5 flex flex-col overflow-hidden ${
+        colourVariants[colour]
+      } rounded px-3 py-2`}
     >
       <h1 className="font-medium">{title}</h1>
       <p className="text-sm font-normal">
