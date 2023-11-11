1ece24e6a922389d966da50b6eb388749a431ec4
s3943811 <s3943811@student.rmit.edu.au>
2023-11-11 15:35:57 +1100

    Add calendar view with time slots and events

--- src/app/(Planner)/Calendar.tsx
+++ src/app/(Planner)/Calendar.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { Timeline, Planner } from "~/components";
+import { CalendarHeader, TimeSlot, Event } from "~/components";
+import { ClassListData } from "~/data/data";
 
 function Calendar() {
   return (
     <section className="flex w-full grow flex-row">
-      {/* <Timeline /> */}
-      <Planner />
+      <div className="grid w-full grid-cols-[auto,repeat(5,1fr)] grid-rows-[2.5rem,repeat(38,50px)]">
+        <CalendarHeader />
+        {Array.from({ length: 38 }, (_, index) => (
+          <React.Fragment key={index}>
+            <div
+              className={`sticky left-0 col-start-[1] ${
+                "row-start-[" + [2 + index] + "]"
+              } border-r border-slate-100 bg-white p-1.5 text-right text-xs font-medium uppercase text-slate-400`}
+            >
+              {index % 2 !== 1 && `${index / 2 + 5}:00`}
+            </div>
+            <TimeSlot col={2} row={2 + index} />
+            <TimeSlot col={3} row={2 + index} />
+            <TimeSlot col={4} row={2 + index} />
+            <TimeSlot col={5} row={2 + index} />
+            <TimeSlot col={6} row={2 + index} />
+          </React.Fragment>
+        ))}
+        {ClassListData.map((item) => (
+          <React.Fragment key={item.courseCode + item.type}>
+            <Event
+              title={item.title}
+              type={item.type}
+              colour={item.colour}
+              time={item.options[0]!}
+            />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
 }
--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,34 +1,84 @@
 export type Course = {
-    title: string,
-    courseCode: string,
-    type: CourseType,
-    colour: ColourPalette,
-    options: Array<Time>
-}
+  title: string;
+  courseCode: string;
+  type: CourseType;
+  colour: ColourPalette;
+  options: Array<Time>;
+};
 
 export type Time = {
-    day: string,
-    start: string,
-    duration: number,
-    location: string,
-    campus_description: string,
-}
+  day: string;
+  start: string;
+  duration: number;
+  location: string;
+  campus_description: string;
+};
 export enum CourseType {
-    Lecture,
-    Tutorial,
+  Lecture,
+  Tutorial,
 }
 
 export enum Days {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
 }
 
 export enum ColourPalette {
-    purple,
-    yellow,
-    orange,
-    red
+  purple,
+  yellow,
+  orange,
+  red,
 }
+
+export const colStart: Record<number, string> = {
+  1: "col-start-[1]",
+  2: "col-start-[2]",
+  3: "col-start-[3]",
+  4: "col-start-[4]",
+  5: "col-start-[5]",
+  6: "col-start-[6]",
+};
+export const rowStart: Record<number, string> = {
+  1: "row-start-[1]",
+  2: "row-start-[2]",
+  3: "row-start-[3]",
+  4: "row-start-[4]",
+  5: "row-start-[5]",
+  6: "row-start-[6]",
+  7: "row-start-[7]",
+  8: "row-start-[8]",
+  9: "row-start-[9]",
+  10: "row-start-[10]",
+  11: "row-start-[11]",
+  12: "row-start-[12]",
+  13: "row-start-[13]",
+  14: "row-start-[14]",
+  15: "row-start-[15]",
+  16: "row-start-[16]",
+  17: "row-start-[17]",
+  18: "row-start-[18]",
+  19: "row-start-[19]",
+  20: "row-start-[20]",
+  21: "row-start-[21]",
+  22: "row-start-[22]",
+  23: "row-start-[23]",
+  24: "row-start-[24]",
+  25: "row-start-[25]",
+  26: "row-start-[26]",
+  27: "row-start-[27]",
+  28: "row-start-[28]",
+  29: "row-start-[29]",
+  30: "row-start-[30]",
+  31: "row-start-[31]",
+  32: "row-start-[32]",
+  33: "row-start-[33]",
+  34: "row-start-[34]",
+  35: "row-start-[35]",
+  36: "row-start-[36]",
+  37: "row-start-[37]",
+  38: "row-start-[38]",
+  39: "row-start-[39]",
+};
