e0488f82b09edf75fdae4f2ab2eb0e0961685e73
s3943811 <s3943811@student.rmit.edu.au>
2023-11-12 23:05:58 +1100

    Add drop functionality for course preferences and store data in url

--- src/components/Calendar/Event/PreviewEventClient.tsx
+++ src/components/Calendar/Event/PreviewEventClient.tsx
@@ -1,28 +1,40 @@
 "use client";
 import { usePreview } from "~/contexts/PreviewContext";
-import { colStart, rowStart, rowSpans, Time } from "~/lib/definitions";
+import { colStart, rowStart, rowSpans, Time, Course } from "~/lib/definitions";
 import { getDayEnum, getRowIndex } from "~/lib/functions";
 import React from "react";
+import { useDroppable } from "@dnd-kit/core";
 
 export default function PreviewEventClient() {
   const { activeCourse } = usePreview();
-
   return activeCourse?.options.map((time: Time) => (
     <React.Fragment key={time.day + time.start + time.duration + time.location}>
-      <PreviewEvent time={time} />
+      <PreviewEvent time={time} course={activeCourse} />
     </React.Fragment>
   ));
 }
 
-function PreviewEvent({ time }: { time: Time }) {
+function PreviewEvent({ time, course }: { time: Time; course: Course }) {
   const rowSpan: number = time.duration / 30;
+  const { setNodeRef, isOver } = useDroppable({
+    id: time.day + time.start + time.duration + time.location,
+    data: {
+      accepts: course,
+      time: time,
+    },
+  });
   return (
     <div
+      ref={setNodeRef}
       className={`${colStart[getDayEnum(time.day)! + 2]} ${
         rowStart[getRowIndex(time.start)]
-      } ${
-        rowSpans[rowSpan]
-      } m-0.5 rounded border-[0.18rem] border-dashed bg-slate-300/20  px-3 py-2 hover:cursor-copy`}
+      } ${rowSpans[rowSpan]}
+      ${
+        isOver
+          ? "border-[0.19rem] border-dashed border-green-500 bg-green-500/40"
+          : "border-[0.18rem] border-dashed bg-slate-300/20"
+      }
+       m-0.5 rounded px-3 py-2 hover:cursor-copy`}
     ></div>
   );
 }
--- src/contexts/DndProvider.tsx
+++ src/contexts/DndProvider.tsx
@@ -1,14 +1,47 @@
 "use client";
 import { DndContext, DragEndEvent, DragStartEvent } from "@dnd-kit/core";
-import { useState } from "react";
+import { useCallback } from "react";
 import { usePreview } from "./PreviewContext";
 import { restrictToWindowEdges } from "@dnd-kit/modifiers";
+import { Preference } from "~/lib/definitions";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 export function DndProvider({ children }: { children: React.ReactNode }) {
   const { setActiveCourse } = usePreview();
-  const [isDropped, setIsDropped] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
   function handleDragEnd(event: DragEndEvent) {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
+    const { active, over } = event;
+    if (over && over.data.current?.accepts === active.data.current?.course) {
+      const preference: Preference = {
+        title: active.data.current?.course.title,
+        courseCode: active.data.current?.course.courseCode,
+        type: active.data.current?.course.type,
+        colour: active.data.current?.course.colour,
+        time: over.data.current?.time,
+      };
+      const newPref = createQueryString(
+        "pref",
+        encodeURIComponent(JSON.stringify(preference)),
+      );
+      const currentPref = searchParams.get("pref");
+      if (currentPref) {
+        const url = `${pathname}?${searchParams}&${newPref}`;
+        console.log(url);
+        router.push(url);
+      } else {
+        console.log("3");
+        router.push(`${pathname}?${searchParams}&${newPref}`);
+      }
     }
     setActiveCourse(null);
   }
