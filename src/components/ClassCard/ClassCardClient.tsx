a1cfb356f2458a80f0eea8ad3220d2b1d9176930
s3943811 <s3943811@student.rmit.edu.au>
2023-11-12 23:07:12 +1100

    Add usePreview to ClassCardClient to disable drag when event has been dropped onto event

--- src/components/ClassCard/ClassCardClient.tsx
+++ src/components/ClassCard/ClassCardClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Course, colourVariants } from "~/lib/definitions";
 import { useDraggable } from "@dnd-kit/core";
-
+import { usePreview } from "~/contexts/PreviewContext";
 export default function ClassCardClient({
   children,
   course,
@@ -9,18 +9,25 @@ export default function ClassCardClient({
   children: React.ReactNode;
   course: Course;
 }) {
-  const { attributes, listeners, setNodeRef, transform, isDragging } =
-    useDraggable({
-      id: course.courseCode + course.type,
-      data: {
-        course: course,
-      },
-      attributes: {
-        role: "div",
-        tabIndex: 0,
-      },
-    });
-
+  const { events } = usePreview();
+  const disabled = events.some(
+    (pref) =>
+      pref.title === course.title &&
+      pref.courseCode === course.courseCode &&
+      pref.type === course.type &&
+      pref.colour === course.colour,
+  );
+  const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
+    id: course.courseCode + course.type,
+    data: {
+      course: course,
+    },
+    attributes: {
+      role: "div",
+      tabIndex: 0,
+    },
+    disabled: disabled,
+  });
   return (
     <div
       ref={setNodeRef}
@@ -28,7 +35,9 @@ export default function ClassCardClient({
       {...attributes}
       className={`${
         isDragging && "opacity-50"
-      } flex w-72 flex-row gap-1 rounded-md border-r-[6.5px] px-2.5 py-2.5 shadow-sm hover:cursor-grab hover:bg-stone-100 ${
+      } flex w-72 flex-row gap-1 rounded-md border-r-[6.5px] px-2.5 py-2.5 shadow-sm ${
+        disabled ? "hover:cursor-no-drop" : ""
+      } hover:bg-stone-100 ${
         colourVariants[course.colour]
       } items-center focus:ring-1 focus:ring-stone-200 active:cursor-grab active:bg-stone-100`}
       tabIndex={0}
