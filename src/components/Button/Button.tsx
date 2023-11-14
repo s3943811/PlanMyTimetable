b9bbfb81fb7891994c716f8b7bf4d1784a21058d
s3943811 <s3943811@student.rmit.edu.au>
2023-11-14 17:20:17 +1100

    Refactor Button component to accept variant prop.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,29 @@
-import { MouseEventHandler } from "react";
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-  type?: "submit" | "reset" | "button" | undefined;
+import { forwardRef } from "react";
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "normal" | "outline" | undefined;
 }
 
-function Button({ children, onClick, type }: ButtonProps) {
-  return (
-    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background bg-neutral-900 text-neutral-50 hover:bg-neutral-800 h-8 px-4 py-2 w-fit" onClick={onClick} type={type}>
-      {children}
-    </button>
-  );
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant, ...props }, ref) => {
+    {
+      /* focus:ring-2 focus:ring-neutral-400/50 focus:ring-offset-2 focus:ring-offset-neutral-50 */
+    }
+    const buttonVariants = {
+      normal:
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background bg-neutral-900 text-neutral-50 hover:bg-neutral-800 h-8 px-4 py-2 w-fit",
+      outline:
+        "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md border border-neutral-200 px-4 py-2 text-sm hover:bg-neutral-50 ",
+    };
+    return (
+      <button
+        ref={ref}
+        className={buttonVariants[variant ?? "normal"]}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 export default Button;
