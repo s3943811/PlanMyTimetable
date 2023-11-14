6fc6185a58c6f790585e99b3e77ffdc57a8ede5c
s3943811 <s3943811@student.rmit.edu.au>
2023-11-14 15:56:43 +1100

    Update ClassListSidebar component UI

--- src/app/(Planner)/ClassListSidebar.tsx
+++ src/app/(Planner)/ClassListSidebar.tsx
@@ -6,15 +6,12 @@ import {
 } from "~/components";
 import { CourseType } from "~/lib/definitions";
 import { ClassListData } from "~/data/data";
-import { LiaBookSolid } from "react-icons/lia";
 
 export default function ClassListSidebar() {
   return (
     <aside className="sticky top-0 flex h-screen min-h-screen w-64 min-w-fit flex-col gap-3 overflow-y-auto overflow-x-hidden border-r p-3">
-      <div className=" inline-flex px-3 py-1 font-medium">
-        <h2 className="text-md inline-flex items-center gap-1">
-          <LiaBookSolid size={20.8} /> Courses
-        </h2>
+      <div className=" inline-flex border-b border-b-neutral-50 px-3 py-1 font-medium">
+        <h2 className="inline-flex items-center gap-1 text-md">Classes</h2>
       </div>
       {ClassListData.map((item) => (
         <ClassCardClient
