3632a9934dafe81931dbcf675d9bae0225d05392
s3943811 <s3943811@student.rmit.edu.au>
2023-11-14 16:36:01 +1100

    Add "Add Class" button to ClassListSidebar component

--- src/app/(Planner)/ClassListSidebar.tsx
+++ src/app/(Planner)/ClassListSidebar.tsx
@@ -6,12 +6,20 @@ import {
 } from "~/components";
 import { CourseType } from "~/lib/definitions";
 import { ClassListData } from "~/data/data";
+import { HiPlus, HiOutlinePlusCircle } from "react-icons/hi";
 
 export default function ClassListSidebar() {
   return (
     <aside className="sticky top-0 flex h-screen min-h-screen w-64 min-w-fit flex-col gap-3 overflow-y-auto overflow-x-hidden border-r p-3">
-      <div className=" inline-flex border-b border-b-neutral-50 px-3 py-1 font-medium">
-        <h2 className="inline-flex items-center gap-1 text-md">Classes</h2>
+      <div className=" inline-flex w-72 justify-between border-b border-b-neutral-50 py-1 pl-3 pr-1">
+        <h2 className="inline-flex items-center gap-1 text-md font-medium">
+          Classes
+        </h2>
+        {/* focus:ring-2 focus:ring-neutral-400/50 focus:ring-offset-2 focus:ring-offset-neutral-50 */}
+        <button className="inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md border border-neutral-200 px-4 py-2 text-sm hover:bg-neutral-50 ">
+          <HiOutlinePlusCircle />
+          Add
+        </button>
       </div>
       {ClassListData.map((item) => (
         <ClassCardClient
