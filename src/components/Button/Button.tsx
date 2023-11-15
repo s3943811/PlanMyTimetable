3e20498e4c63e760c59edefde415ec941cf11bac
s3943811 <s3943811@student.rmit.edu.au>
2023-11-15 23:19:22 +1100

    Add new button variants to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,30 @@
 import { forwardRef } from "react";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "normal" | "outline" | "outlineIcon" | "ghost" | undefined;
+  variant?:
+    | "normal"
+    | "normalLarge"
+    | "secondary"
+    | "outline"
+    | "outlineIcon"
+    | "ghostIcon"
+    | "ghost"
+    | undefined;
 }
 export const buttonVariants = {
   normal:
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background bg-neutral-900 text-neutral-50 hover:bg-neutral-800 h-8 px-4 py-2 w-fit",
+    "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md bg-neutral-900 shadow px-4 py-2 text-sm text-neutral-50 hover:bg-neutral-700 ",
+  secondary:
+    "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md bg-neutral-100/80 shadow-sm px-4 py-2 text-sm hover:bg-neutral-100 ",
   outline:
     "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md border border-neutral-200 px-4 py-2 text-sm hover:bg-neutral-50 ",
   outlineIcon:
     "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md border border-neutral-200 px-2.5 py-2 text-sm hover:bg-neutral-50 ",
-  ghost:
+  ghostIcon:
     "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md px-2.5 py-2 text-sm hover:bg-neutral-50 active:bg-neutral-100",
+  ghost:
+    "inline-flex h-8 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md px-4 py-2 text-sm hover:bg-neutral-50 active:bg-neutral-100",
+  normalLarge:
+    "inline-flex h-10 w-fit items-center justify-center gap-1 whitespace-nowrap rounded-md bg-neutral-900 shadow px-4 py-2 text-base text-neutral-50 hover:bg-neutral-700 ",
 };
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
