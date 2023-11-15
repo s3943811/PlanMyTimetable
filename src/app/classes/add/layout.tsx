ad326d2e1ce1f686e377d32c4a87047f4ea3a34a
s3943811 <s3943811@student.rmit.edu.au>
2023-11-15 23:20:15 +1100

    Add layout component for add page

---
+++ src/app/classes/add/layout.tsx
@@ -0,0 +1,16 @@
+export default function AddLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="container flex grow flex-col p-8">
+      <h2 className="text-xl font-medium tracking-tight">Add classes</h2>
+      <p className="font-light text-neutral-500/90">
+        Add class details, times, and locations.
+      </p>
+      <div
+        data-orientation="horizontal"
+        role="none"
+        className=" my-4 h-[1px] w-full shrink-0 bg-neutral-200"
+      ></div>
+      {children}
+    </section>
+  );
+}
