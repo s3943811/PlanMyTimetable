87ab11e1eae178a7780c657a0b04eb78b919143a
s3943811 <s3943811@student.rmit.edu.au>
2023-11-15 14:28:56 +1100

    Add RetainParamsLink to keep params when navigating home

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { HiCalendar, HiOutlineAdjustments } from "react-icons/hi";
-import Button from "../Button/Button";
+import { RetainLink, Button } from "~/components";
 function Navbar() {
   return (
     <nav className="sticky top-0 flex h-screen flex-col items-center justify-between border-r bg-white p-3 pt-8">
       <div>
-        <Link
+        <RetainLink
           href="/"
           className="flex flex-row items-center justify-center gap-2 rounded-lg px-4 py-1.5 font-medium hover:bg-neutral-100 active:bg-neutral-200"
         >
           <HiCalendar />
           Home
-        </Link>
+        </RetainLink>
       </div>
       <Button variant="outline">
         <HiOutlineAdjustments />
