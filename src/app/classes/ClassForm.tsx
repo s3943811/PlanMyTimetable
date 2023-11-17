b265803a27aee89212f36b43875b1a2fd47abaf0
s3943811 <s3943811@student.rmit.edu.au>
2023-11-18 01:08:41 +1100

    Add zod schema and react hook form for form validation and error messages

--- src/app/classes/ClassForm.tsx
+++ src/app/classes/ClassForm.tsx
@@ -1,12 +1,77 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useId } from "react";
-import { Button } from "~/components";
-import { HiOutlineSelector, HiOutlinePlusCircle } from "react-icons/hi";
+"use client";
+import {
+  useForm,
+  useFieldArray,
+  UseFormRegister,
+  UseFieldArrayRemove,
+  FieldErrors,
+} from "react-hook-form";
+import { Button, Tooltip } from "~/components";
+import {
+  HiOutlineXCircle,
+  HiOutlinePlusCircle,
+  HiOutlineMinusCircle,
+} from "react-icons/hi";
 import { CourseType } from "~/lib/definitions";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const optionSchema = z.object({
+  day: z.enum(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]),
+  start_time: z
+    .string()
+    .regex(
+      /^([0-1][0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/,
+      "Invalid time format",
+    ),
+  room: z.string().min(1, { message: "Room is required" }),
+  campus: z.string().min(1, { message: "Campus is required" }),
+});
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "Name is required" })
+    .max(120, { message: "Name must be less than 120 characters" }),
+  code: z.string(),
+  duration: z.coerce
+    .number()
+    .gte(1, { message: "Duration must be at least 1 minute." })
+    .lte(600, { message: "Duration must be less than than 600 minutes." }),
+  options: z
+    .array(optionSchema)
+    .min(1, { message: "At least one option is required" }),
+});
 
 export default function ClassForm() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      options: [
+        {
+          day: "Monday",
+          start_time: "",
+          room: "",
+          campus: "",
+        },
+      ],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "options",
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // TODO: Deal with this
+    console.log(values);
+  }
   return (
-    <form className="contents space-y-7">
+    <form className="contents space-y-7" onSubmit={handleSubmit(onSubmit)}>
       <div className=" space-y-2">
         <label
           className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -15,14 +80,20 @@ export default function ClassForm() {
           Name
         </label>
         <input
-          required
+          {...register("name")}
           id="course_name"
           placeholder="Name"
-          className=" flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm file:border-0 
-          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 
-          focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
-          disabled:cursor-not-allowed disabled:opacity-50"
+          className={`flex h-10 w-full rounded-md border ${
+            errors.name && "border-red-300"
+          } px-3 py-2 text-sm shadow-sm file:border-0 
+          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 ${
+            errors.name
+              ? " focus:ring-red-400/60"
+              : " focus:ring-neutral-400/60"
+          } focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+          disabled:cursor-not-allowed disabled:opacity-50`}
         />
+        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         <p className="text-xs font-light text-neutral-500/90">
           This is the name of the class in plain text. For example, Full Stack
           Development.
@@ -36,6 +107,7 @@ export default function ClassForm() {
           Code
         </label>
         <input
+          {...register("code")}
           id="course_code"
           placeholder="Code"
           className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm 
@@ -43,87 +115,47 @@ export default function ClassForm() {
           placeholder:text-neutral-400/90 focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
           disabled:cursor-not-allowed disabled:opacity-50"
         />
+        {errors.code && <ErrorMessage>{errors.code.message}</ErrorMessage>}
         <p className="text-xs font-light text-neutral-500/90">
           This is a code used to describe the course by the university
           (optional). For example, COSC2758.
         </p>
       </div>
       {/* TODO: Placeholder here to create tabs */}
-      <div className="h-10 space-y-2 rounded-md bg-neutral-200"></div>
-      <div className="inline-flex justify-between">
+      {/* <div className="h-10 space-y-2 rounded-md bg-neutral-200"></div> */}
+      <div className="sticky top-0 z-50 inline-flex justify-between bg-white">
         <div className="space-y-1">
           <h3 className="text-lg font-medium">Options</h3>
           <p className="text-xs font-light text-neutral-500/90">
             This is the times and rooms on offer for this class this semester.
           </p>
         </div>
-        <Button variant="secondary" type="button">
-          <HiOutlinePlusCircle /> Add Option
-        </Button>
+        <div className=" inline-flex gap-1">
+          <Tooltip message={"Remove all"}>
+            <Button
+              variant="secondaryIcon"
+              type="button"
+              onClick={() => {
+                for (let i = fields.length - 1; i > 0; i--) {
+                  remove(i);
+                }
+              }}
+            >
+              <HiOutlineXCircle />
+            </Button>
+          </Tooltip>
+          <Button
+            variant="secondary"
+            type="button"
+            onClick={() =>
+              append({ day: "Monday", start_time: "", room: "", campus: "" })
+            }
+          >
+            <HiOutlinePlusCircle /> Add Option
+          </Button>
+        </div>
       </div>
       <div className=" space-y-6">
-        <OptionForm type={CourseType.Lecture} />
-        <div
-          data-orientation="horizontal"
-          role="none"
-          className=" my-4 h-[1px] w-full shrink-0 bg-neutral-300"
-        ></div>
-        <OptionForm type={CourseType.Lecture} />
-      </div>
-      <div className="ml-auto space-x-5">
-        {/* <Button variant="outline">Cancel</Button> */}
-        <Button variant="normalLarge">Submit</Button>
-      </div>
-    </form>
-  );
-}
-
-function OptionForm({ type }: { type: CourseType }) {
-  return (
-    <div className="space-y-7 rounded-md border px-2.5 py-2 pb-3">
-      <div className=" space-y-2">
-        <label
-          className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          htmlFor="option_day"
-        >
-          Day
-        </label>
-        <select
-          className=" flex h-10 w-full appearance-none rounded-md border bg-white px-3 py-2 text-sm shadow-sm
-          disabled:cursor-not-allowed disabled:opacity-50"
-        >
-          <option>Monday</option>
-          <option>Tuesday</option>
-          <option>Wednesday</option>
-          <option>Thursday</option>
-          <option>Friday</option>
-        </select>
-        <p className="text-xs font-light text-neutral-500/90">
-          The day of the week.
-        </p>
-      </div>
-      <div className="flex flex-row space-x-10">
-        <div className=" w-full space-y-2">
-          <label
-            className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            htmlFor="start_time"
-          >
-            Start Time
-          </label>
-          <input
-            required
-            id="start_time"
-            placeholder="Start time"
-            type="time"
-            className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm 
-          file:border-0 file:bg-transparent file:font-medium
-          placeholder:text-neutral-400/90 focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
-          disabled:cursor-not-allowed disabled:opacity-50"
-          />
-          <p className="text-xs font-light text-neutral-500/90">
-            The time the {CourseType[type]} starts.
-          </p>
-        </div>
         <div className=" w-full space-y-2">
           <label
             className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -132,66 +164,208 @@ function OptionForm({ type }: { type: CourseType }) {
             Duration
           </label>
           <input
-            required
+            {...register("duration")}
             id="duration"
             placeholder="Duration"
             type="number"
-            min={1}
-            max={600}
-            className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm 
-          file:border-0 file:bg-transparent file:font-medium
-          placeholder:text-neutral-400/90 focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
-          disabled:cursor-not-allowed disabled:opacity-50"
+            className={`flex h-10 w-full rounded-md border ${
+              errors.duration && "border-red-300"
+            } px-3 py-2 text-sm shadow-sm file:border-0 
+          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 ${
+            errors.duration
+              ? " focus:ring-red-400/60"
+              : " focus:ring-neutral-400/60"
+          } focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+          disabled:cursor-not-allowed disabled:opacity-50`}
           />
+          {errors.duration && (
+            <ErrorMessage>{errors.duration.message}</ErrorMessage>
+          )}
           <p className="text-xs font-light text-neutral-500/90">
-            How long the {CourseType[type]} goes for in minutes.
+            How long the {CourseType[CourseType.Lecture]} goes for in minutes.
           </p>
         </div>
+        {errors.options && (
+          <ErrorMessage>{errors.options.message}</ErrorMessage>
+        )}
+        {fields.map((fields, index) => (
+          <div key={fields.id}>
+            <OptionForm
+              type={CourseType.Lecture}
+              register={register}
+              errors={errors}
+              index={index}
+              remove={remove}
+            />
+          </div>
+        ))}
       </div>
+      <div className="ml-auto space-x-5">
+        <Button variant="normalLarge">Submit</Button>
+      </div>
+    </form>
+  );
+}
 
-      <div className="flex flex-row space-x-10">
-        <div className=" w-full space-y-2">
-          <label
-            className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            htmlFor="room"
-          >
-            Room
-          </label>
-          <input
-            required
-            id="room"
-            placeholder="Room"
-            className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm 
-          file:border-0 file:bg-transparent file:font-medium
-          placeholder:text-neutral-400/90 focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
-          disabled:cursor-not-allowed disabled:opacity-50"
-          />
-          <p className="text-xs font-light text-neutral-500/90">
-            What room the {CourseType[type]} will be in (if online write "-").
-          </p>
+function OptionForm({
+  type,
+  register,
+  errors,
+  index,
+  remove,
+}: {
+  type: CourseType;
+  register: UseFormRegister<z.infer<typeof formSchema>>;
+  errors: FieldErrors<z.infer<typeof formSchema>>;
+  index: number;
+  remove: UseFieldArrayRemove;
+}) {
+  return (
+    <div className="relative rounded-md border px-2.5 py-2 pb-3">
+      {index !== 0 && (
+        <div className="absolute right-1 top-1">
+          <Tooltip message="Remove">
+            <Button variant="ghostIcon" onClick={() => remove(index)}>
+              <HiOutlineMinusCircle />
+            </Button>
+          </Tooltip>
         </div>
-        <div className=" w-full space-y-2">
-          <label
-            className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            htmlFor="campus"
-          >
-            Campus
-          </label>
-          <input
-            required
-            id="room"
-            placeholder="Room"
-            className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm 
-          file:border-0 file:bg-transparent file:font-medium
-          placeholder:text-neutral-400/90 focus:ring-neutral-400/60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+      )}
+      <div className="space-y-7">
+        <div className="flex flex-row space-x-10">
+          <div className=" w-full space-y-2">
+            <label
+              className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="option_day"
+            >
+              Day
+            </label>
+
+            <select
+              className=" flex h-10 w-full appearance-none rounded-md border bg-white px-3 py-2 text-sm shadow-sm
           disabled:cursor-not-allowed disabled:opacity-50"
-          />
-          <p className="text-xs font-light text-neutral-500/90">
-            What campus the {CourseType[type]} will be on. For example, RMIT
-            students may write "Melbourne City" or "Canvas".
-          </p>
+              {...register(`options.${index}.day`)}
+            >
+              <option>Monday</option>
+              <option>Tuesday</option>
+              <option>Wednesday</option>
+              <option>Thursday</option>
+              <option>Friday</option>
+            </select>
+            {errors.options && (
+              <ErrorMessage>{errors.options[index]?.day?.message}</ErrorMessage>
+            )}
+            <p className="text-xs font-light text-neutral-500/90">
+              The day of the week.
+            </p>
+          </div>
+          <div className=" w-full space-y-2">
+            <label
+              className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="start_time"
+            >
+              Start Time
+            </label>
+            <input
+              {...register(`options.${index}.start_time`)}
+              id="start_time"
+              placeholder="Start time"
+              type="time"
+              className={`flex h-10 w-full rounded-md border ${
+                errors.options &&
+                errors.options[index]?.start_time &&
+                "border-red-300"
+              } px-3 py-2 text-sm shadow-sm file:border-0 
+          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 ${
+            errors.options && errors.options[index]?.start_time
+              ? " focus:ring-red-400/60"
+              : " focus:ring-neutral-400/60"
+          } focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+          disabled:cursor-not-allowed disabled:opacity-50`}
+            />
+            {errors.options && (
+              <ErrorMessage>
+                {errors.options[index]?.start_time?.message}
+              </ErrorMessage>
+            )}
+            <p className="text-xs font-light text-neutral-500/90">
+              The time the {CourseType[type]} starts.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-row space-x-10">
+          <div className=" w-full space-y-2">
+            <label
+              className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="room"
+            >
+              Room
+            </label>
+            <input
+              {...register(`options.${index}.room`)}
+              id="room"
+              placeholder="Room"
+              className={`flex h-10 w-full rounded-md border ${
+                errors.options &&
+                errors.options[index]?.room &&
+                "border-red-300"
+              } px-3 py-2 text-sm shadow-sm file:border-0 
+          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 ${
+            errors.options && errors.options[index]?.room
+              ? " focus:ring-red-400/60"
+              : " focus:ring-neutral-400/60"
+          } focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+          disabled:cursor-not-allowed disabled:opacity-50`}
+            />
+            {errors.options && (
+              <ErrorMessage>
+                {errors.options[index]?.room?.message}
+              </ErrorMessage>
+            )}
+            <p className="text-xs font-light text-neutral-500/90">
+              What room the {CourseType[type]} will be in (if online write "-").
+            </p>
+          </div>
+          <div className=" w-full space-y-2">
+            <label
+              className="ml-0.5 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="campus"
+            >
+              Campus
+            </label>
+            <input
+              {...register(`options.${index}.campus`)}
+              id="campus"
+              placeholder="Campus"
+              className={`flex h-10 w-full rounded-md border ${
+                errors.options &&
+                errors.options[index]?.campus &&
+                "border-red-300"
+              } px-3 py-2 text-sm shadow-sm file:border-0 
+          file:bg-transparent file:font-medium placeholder:text-neutral-500/90 ${
+            errors.options && errors.options[index]?.campus
+              ? " focus:ring-red-400/60"
+              : " focus:ring-neutral-400/60"
+          } focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1
+          disabled:cursor-not-allowed disabled:opacity-50`}
+            />
+            {errors.options && (
+              <ErrorMessage>
+                {errors.options[index]?.campus?.message}
+              </ErrorMessage>
+            )}
+            <p className="text-xs font-light text-neutral-500/90">
+              What campus the {CourseType[type]} will be on. For example, RMIT
+              students may write "Melbourne City" or "Canvas".
+            </p>
+          </div>
         </div>
       </div>
     </div>
   );
+}
+
+function ErrorMessage({ children }: { children: React.ReactNode }) {
+  return <p className="text-sm text-red-500">{children}</p>;
 }
