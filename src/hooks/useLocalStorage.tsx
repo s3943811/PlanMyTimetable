3b1fbdaa97095a2fbc7b16fed047a07259fe2199
s3943811 <s3943811@student.rmit.edu.au>
2023-11-25 21:25:19 +1100

    Add useLocalStorage hook for storing data in local storage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    if (typeof window === "undefined") {
+      return typeof initialValue === "function"
+        ? (initialValue as () => T)()
+        : initialValue;
+    }
+
+    const storedValue = window.localStorage.getItem(key);
+    return storedValue !== null
+      ? JSON.parse(storedValue)
+      : typeof initialValue === "function"
+      ? (initialValue as () => T)()
+      : initialValue;
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
