0c0e3b752cf92ff8d3bdd03223e18ee54628a18f
s3943811 <s3943811@student.rmit.edu.au>
2023-12-03 00:36:13 +1100

    Fix performance issues with useCallback on urlState functions

--- src/hooks/useUrlState.tsx
+++ src/hooks/useUrlState.tsx
@@ -24,68 +24,75 @@ export function useUrlState() {
   // This method will take an state array encoding in the url and add the new item to that array
   // re-encode and push to url
   // if that item exists
-  const appendState = (
-    element: unknown,
-    prefName: string,
-    location: string = pathname,
-  ) => {
-    const state = decode(prefName) as Array<unknown>;
-    let encoded;
-    const params = new URLSearchParams(searchParams);
-    if (state) {
-      state.push(element);
-      encoded = JSONCrush.crush(JSON.stringify(state));
-      params.delete(prefName);
-    } else if (Array.isArray(element)) {
-      encoded = JSONCrush.crush(JSON.stringify(element));
-    } else {
-      encoded = JSONCrush.crush(JSON.stringify([element]));
-    }
+  const appendState = useCallback(
+    (element: unknown, prefName: string, location: string = pathname) => {
+      const state = decode(prefName) as Array<unknown>;
+      let encoded;
+      const params = new URLSearchParams(searchParams);
+      if (state) {
+        state.push(element);
+        encoded = JSONCrush.crush(JSON.stringify(state));
+        params.delete(prefName);
+      } else if (Array.isArray(element)) {
+        encoded = JSONCrush.crush(JSON.stringify(element));
+      } else {
+        encoded = JSONCrush.crush(JSON.stringify([element]));
+      }
 
-    const query = createQueryString(prefName, encoded);
-    router.push(`${location}?${query}`, { scroll: false });
-  };
+      const query = createQueryString(prefName, encoded);
+      router.push(`${location}?${query}`, { scroll: false });
+    },
+    [searchParams, router, pathname],
+  );
 
   // This method will replace the state in the url with the new element
-  const replaceState = (
-    element: unknown,
-    prefName: string,
-    location: string = pathname,
-  ): void => {
-    // console.log(searchParams.toString());
+  const replaceState = useCallback(
+    (element: unknown, prefName: string, location: string = pathname): void => {
+      // console.log(searchParams.toString());
 
-    const newState = JSONCrush.crush(JSON.stringify(element));
-    const newPref = createQueryString(prefName, newState);
+      const newState = JSONCrush.crush(JSON.stringify(element));
+      const newPref = createQueryString(prefName, newState);
 
-    router.push(`${location}?${newPref}`, { scroll: false });
-  };
+      router.push(`${location}?${newPref}`, { scroll: false });
+    },
+    [searchParams, router, pathname],
+  );
 
   // This will replace all the states that are passed similar to replaceState
-  const replaceMultiple = (
-    elements: { element: unknown; prefName: string }[],
-    location: string = pathname,
-  ) => {
-    const currParams = new URLSearchParams(searchParams);
-    const encoded = elements.map((item) => {
-      return {
-        element: JSONCrush.crush(JSON.stringify(item.element)),
-        prefName: item.prefName,
-      };
-    });
-    encoded.map((item) => currParams.set(item.prefName, item.element));
-    router.push(`${location}?${currParams.toString()}`, { scroll: false });
-  };
+  const replaceMultiple = useCallback(
+    (
+      elements: { element: unknown; prefName: string }[],
+      location: string = pathname,
+    ) => {
+      const currParams = new URLSearchParams(searchParams);
+      const encoded = elements.map((item) => {
+        return {
+          element: JSONCrush.crush(JSON.stringify(item.element)),
+          prefName: item.prefName,
+        };
+      });
+      encoded.map((item) => currParams.set(item.prefName, item.element));
+      router.push(`${location}?${currParams.toString()}`, { scroll: false });
+    },
+    [searchParams, router, pathname],
+  );
 
   // decode url states
-  const decode = (pref: string): unknown => {
-    const encodedValue = searchParams.get(pref);
-    return encodedValue && JSON.parse(JSONCrush.uncrush(encodedValue));
-  };
+  const decode = useCallback(
+    (pref: string): unknown => {
+      const encodedValue = searchParams.get(pref);
+      return encodedValue && JSON.parse(JSONCrush.uncrush(encodedValue));
+    },
+    [searchParams],
+  );
 
-  const redirect = (location: string) => {
-    const params = new URLSearchParams(searchParams);
-    nextRedirect(`${location}?${params.toString()}`);
-  };
+  const redirect = useCallback(
+    (location: string) => {
+      const params = new URLSearchParams(searchParams);
+      nextRedirect(`${location}?${params.toString()}`);
+    },
+    [searchParams, nextRedirect, pathname],
+  );
 
   return {
     decode,
