3e75be3db7d5a12bd4dc8c8d744ff85bff89d039
s3943811 <s3943811@student.rmit.edu.au>
2023-12-07 00:06:42 +1100

    Update themeSelector styling and add skeleton for hydration purposes

--- src/components/ThemeSelectors/ThemeSelector.tsx
+++ src/components/ThemeSelectors/ThemeSelector.tsx
@@ -1,26 +1,41 @@
 "use client";
 import { useTheme } from "next-themes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Dispatch, SetStateAction } from "react";
 import { HiSun, HiMoon, HiComputerDesktop } from "react-icons/hi2";
 import { Popover } from "react-tiny-popover";
 import { Button } from "~/components";
 
 export default function ThemeSelector() {
+  const [mounted, setMounted] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const { theme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return (
+      <div className=" h-8 animate-pulse rounded-md bg-neutral-100 dark:bg-neutral-700"></div>
+    );
+  }
+
   return (
     <Popover
       containerClassName="z-[999]"
       isOpen={isOpen}
       positions={["right"]}
-      padding={5}
+      padding={10}
       onClickOutside={() => setIsOpen(false)}
       content={<ThemePopover setIsOpen={setIsOpen} />}
     >
-      <Button variant="outlineIcon" onClick={() => setIsOpen(true)}>
-        <HiSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <HiMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </Button>
+      <button
+        className="flex h-8 w-24 flex-row items-center gap-1 whitespace-nowrap rounded-lg px-2.5 py-1.5 text-sm capitalize hover:bg-neutral-100 active:bg-neutral-200 dark:hover:bg-neutral-700 dark:active:bg-neutral-600"
+        onClick={() => setIsOpen(true)}
+      >
+        <HiSun className=" rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <HiMoon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        {theme}
+      </button>
     </Popover>
   );
 }
@@ -32,7 +47,7 @@ function ThemePopover({
 }) {
   const { setTheme } = useTheme();
   return (
-    <ul className="space-y-1 rounded-md border bg-white p-3 dark:border-neutral-700 dark:bg-neutral-900">
+    <ul className="space-y-1 rounded-md border bg-white p-2 dark:border-neutral-700 dark:bg-neutral-900">
       <li>
         <Button
           variant="ghost"
