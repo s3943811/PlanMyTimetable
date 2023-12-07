ffafb4aaf312b83508d2e7491ba416189cccb1a7
s3943811 <s3943811@student.rmit.edu.au>
2023-12-07 23:21:47 +1100

    Refactor styles for classes pages to be responsive; create sheet for classList on mobile, update classlist style accordingly

---
+++ src/app/classes/MobileClassList.tsx
@@ -0,0 +1,49 @@
+"use client";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogClose,
+  DialogTitle,
+} from "@radix-ui/react-dialog";
+import { useState } from "react";
+import { HiBars3 } from "react-icons/hi2";
+import { HiOutlineX } from "react-icons/hi";
+import { Button } from "~/components";
+import ClassList from "./ClassList";
+
+export default function MobileClassList() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghostIcon"
+          className=" p-2 text-lg font-medium sm:hidden"
+        >
+          <HiBars3 />
+        </Button>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogOverlay className="fixed inset-0 z-50 backdrop-brightness-[.50]" />
+        <DialogContent className=" fixed inset-y-0 left-0 z-50 h-full w-3/4 gap-4 overflow-auto border-r bg-white p-2 shadow-lg dark:border-r-neutral-700 dark:bg-neutral-900 sm:max-w-sm">
+          <DialogTitle className="inline-flex items-center gap-1 pl-1 text-md font-medium">
+            Classes
+          </DialogTitle>
+          <ClassList />
+          <DialogClose asChild>
+            <Button
+              variant="ghostIcon"
+              className="absolute right-2 top-2 rounded-lg p-2"
+            >
+              <HiOutlineX />
+            </Button>
+          </DialogClose>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  );
+}
--- src/app/classes/[class]/layout.tsx
+++ src/app/classes/[class]/layout.tsx
@@ -1,6 +1,7 @@
 import { HiOutlinePlusCircle } from "react-icons/hi";
 import { RetainLink, Tooltip, buttonVariants } from "~/components";
 import ClassList from "../ClassList";
+import MobileClassList from "../MobileClassList";
 
 export default function ClassLayout({
   children,
@@ -10,9 +11,13 @@ export default function ClassLayout({
   return (
     <section className="flex w-full grow flex-col">
       <div className="sticky top-0 z-20 inline-flex w-full justify-between border-b bg-white pb-2 pl-5 pr-3 pt-4 dark:border-b-neutral-700 dark:bg-neutral-900 dark:text-white">
-        <h2 className="inline-flex items-center gap-1 pl-1 text-md font-medium">
-          Classes
-        </h2>
+        <div className="inline-flex items-center">
+          <MobileClassList />
+
+          <h2 className="inline-flex items-center gap-1 pl-1 text-md font-medium">
+            Classes
+          </h2>
+        </div>
         <div className="inline-flex gap-1">
           <Tooltip message={"Add"} position="bottom">
             <RetainLink
@@ -25,8 +30,10 @@ export default function ClassLayout({
           </Tooltip>
         </div>
       </div>
-      <div className="flex grow flex-row">
-        <ClassList />
+      <div className="flex grow flex-col-reverse sm:flex-row">
+        <div className="hidden sm:block">
+          <ClassList />
+        </div>
         {children}
       </div>
     </section>
