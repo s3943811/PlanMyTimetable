36d8254133cad8297fd09ed533e45b7b6980fe9b
s3943811 <s3943811@student.rmit.edu.au>
2023-12-07 17:54:56 +1100

    Fix class add page styling and update data gathering instructions

--- src/app/classes/add/page.tsx
+++ src/app/classes/add/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
         <span>/</span>
         <RetainLink
           href="/classes/add/manual"
-          className={`inline-flex w-full items-center justify-center rounded-md py-1.5 pr-3 font-light text-neutral-500/90 transition-all dark:text-neutral-400`}
+          className={`inline-flex w-full items-center justify-center rounded-md py-1.5 pr-3 text-neutral-500/90 transition-all dark:text-neutral-400`}
         >
           <p>Manual</p>
         </RetainLink>
@@ -25,9 +25,10 @@ export default function Page() {
       ></div>
       <div className="space-y-4">
         <h3 className="text-md font-medium">Automatic data gathering</h3>
-        <p className="text-sm">
+        <p className="text-base">
           If your university uses Allocate+ for timetable purposes, you can
-          gather the data automatically using the following steps
+          gather the data automatically using the following steps (currently
+          this is known to work for RMIT Unversity only):
         </p>
         <ol className="list-inside list-decimal space-y-3 text-sm marker:font-medium">
           <li>
@@ -35,16 +36,36 @@ export default function Page() {
           </li>
           <div className="w-full rounded-md border p-2 text-sm dark:border-neutral-600">
             <a
+              id="Bookmarklet"
               href={`javascript:(() => {
     let script = document.createElement("script");
     script.src = "https://planmytimetable.vercel.app/bookmarklet.js";
     document.body.append(script);
 })();`}
             >
-              <code className="prose-code: my-2 p-2">{`function Test() {
-            do stuff }`}</code>
+              <code
+                id="Bookmarklet"
+                className="prose-code: my-2 whitespace-pre p-2"
+              >
+                Drag me to your bookmarks:
+                {`javascript:(() => {
+    let script = document.createElement("script");
+    script.src = "https://planmytimetable.vercel.app/bookmarklet.js";
+    document.body.append(script);
+})();`}
+              </code>
             </a>
           </div>
+          <p>
+            If you have concerns regarding privacy and security,&nbsp;
+            <a
+              href="https://planmytimetable.vercel.app/bookmarklet.js"
+              className="font-medium underline underline-offset-4 after:content-['_↗']"
+            >
+              checkout the source code
+            </a>
+            . All data is stored locally and never leaves your computer.
+          </p>
           <li>Navigate to your Allocate+ system and login.</li>
           <li>Click and run the bookmarklet on this page.</li>
           <li>You will then be redirected to a new page with your data.</li>
