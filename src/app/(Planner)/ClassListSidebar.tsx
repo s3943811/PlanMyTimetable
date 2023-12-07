f15663cc15978f801b9e4c00e383e94a867fbb69
s3943811 <s3943811@student.rmit.edu.au>
2023-12-07 17:54:46 +1100

    Remove edit button from ClassListSidebar

--- src/app/(Planner)/ClassListSidebar.tsx
+++ src/app/(Planner)/ClassListSidebar.tsx
@@ -13,17 +13,17 @@ export default function ClassListSidebar({
           Classes
         </h2>
         <div className="inline-flex gap-1">
-          <Tooltip message={"Edit"} position="bottom">
+          {/* <Tooltip message={"Edit"} position="bottom">
             <RetainLink className={buttonVariants.outlineIcon} href="/classes">
               <HiOutlineAdjustments />
             </RetainLink>
-          </Tooltip>
+          </Tooltip> */}
           <Tooltip message={"Add"} position="bottom">
             <RetainLink
               className={buttonVariants.outlineIcon}
               href="/classes/add"
             >
-              <HiOutlinePlusCircle />
+              <HiOutlinePlusCircle /> Add
             </RetainLink>
           </Tooltip>
         </div>
