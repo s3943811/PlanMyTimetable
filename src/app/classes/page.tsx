9ee1943798b73c5700dab1d9dd02b6c9ee0b5e60
s3943811 <s3943811@student.rmit.edu.au>
2023-12-08 19:22:03 +1100

    Remove unused code and redirect to homepage

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { HiOutlineAcademicCap } from "react-icons/hi2";
-import { RetainLink } from "~/components";
 import { usePreview } from "~/contexts/PreviewContext";
 import { useUrlState } from "~/hooks/useUrlState";
 
@@ -12,17 +10,6 @@ export default function Page() {
     // console.log(redirectURL);
     redirect(redirectURL);
   } else {
-    return (
-      <div className=" flex h-[100dvh] grow flex-col items-center justify-center gap-3 dark:bg-neutral-900">
-        <HiOutlineAcademicCap size={96} />
-        <p>{`You haven't added any classes yet.`} </p>
-        <RetainLink
-          className="under font-medium underline decoration-[1.5px] underline-offset-[7px] transition-all hover:font-semibold"
-          href={"/classes/add"}
-        >
-          Add a class now.
-        </RetainLink>
-      </div>
-    );
+    redirect("/");
   }
 }
