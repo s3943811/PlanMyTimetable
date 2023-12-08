12e6e3f8ec044a5199bd4113235d7ffbd2c478e6
s3943811 <s3943811@student.rmit.edu.au>
2023-12-08 18:42:42 +1100

    Refactor ClassCardClient styling to use cn for better readability and responsiveness

--- src/components/ClassCard/ClassCardClient.tsx
+++ src/components/ClassCard/ClassCardClient.tsx
@@ -4,6 +4,7 @@ import type { Course } from "~/lib/definitions";
 import { useDraggable } from "@dnd-kit/core";
 import { usePreview } from "~/contexts/PreviewContext";
 import { useMemo } from "react";
+import { cn } from "~/lib/utils";
 export default function ClassCardClient({
   children,
   course,
@@ -38,15 +39,18 @@ export default function ClassCardClient({
       ref={setNodeRef}
       {...listeners}
       {...attributes}
-      className={`z-50 ${
-        isDragging && "opacity-50"
-      } flex w-72 flex-row justify-between gap-1 rounded-md border-l-[6.5px] px-2.5 py-2.5 hover:bg-neutral-100 ${
+      className={cn(
+        `z-50 flex grow items-center justify-between gap-1 rounded-md border-l-[6.5px] px-2.5 
+        py-2.5 hover:cursor-grab hover:bg-neutral-100 focus:ring-1 focus:ring-neutral-200 active:cursor-grab 
+        active:bg-neutral-100 dark:hover:bg-neutral-700 dark:focus:ring-neutral-700 
+        dark:active:bg-neutral-600
+         xl:w-72`,
+        isDragging && "opacity-50",
         isAllocated
           ? "bg-neutral-50 dark:bg-neutral-800"
-          : "bg-white dark:bg-neutral-900"
-      } ${
-        colourVariants[course.colour]
-      } items-center hover:cursor-grab focus:ring-1 focus:ring-neutral-200 active:cursor-grab active:bg-neutral-100 dark:hover:bg-neutral-700 dark:focus:ring-neutral-700 dark:active:bg-neutral-600`}
+          : "bg-white dark:bg-neutral-900",
+        colourVariants[course.colour],
+      )}
       tabIndex={0}
     >
       {children}
