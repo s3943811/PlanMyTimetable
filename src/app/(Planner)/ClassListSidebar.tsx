d1d7e275e6a22f0581a1a16ae22957675c954b5e
Maximus Dionyssopoulos <103098176+s3943811@users.noreply.github.com>
2023-12-09 17:10:11 +1100

    Revert "Responsive Design and Dark Mode"

--- src/app/(Planner)/ClassListSidebar.tsx
+++ src/app/(Planner)/ClassListSidebar.tsx
@@ -1,12 +1,5 @@
-import {
-  RetainLink,
-  buttonVariants,
-  Tooltip,
-  AllocatedPopover,
-} from "~/components";
-import { HiOutlinePlusCircle } from "react-icons/hi";
-import SidebarLayout from "./SidebarLayout";
-import MobileClassList from "./MobileClassList";
+import { RetainLink, buttonVariants, Tooltip } from "~/components";
+import { HiOutlineAdjustments, HiOutlinePlusCircle } from "react-icons/hi";
 
 export default function ClassListSidebar({
   children,
@@ -14,27 +7,28 @@ export default function ClassListSidebar({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarLayout>
-      <div className=" inline-flex justify-between border-b px-3 py-4 dark:border-b-neutral-700">
+    <aside className="sticky top-0 flex h-screen min-h-screen w-64 min-w-fit flex-col border-r ">
+      <div className=" inline-flex justify-between border-b px-3 py-4">
         <h2 className="inline-flex items-center gap-1 text-md font-medium">
-          <MobileClassList />
           Classes
         </h2>
         <div className="inline-flex gap-1">
-          <div className="md:hidden">
-            <AllocatedPopover />
-          </div>
+          <Tooltip message={"Edit"} position="bottom">
+            <RetainLink className={buttonVariants.outlineIcon} href="/classes">
+              <HiOutlineAdjustments />
+            </RetainLink>
+          </Tooltip>
           <Tooltip message={"Add"} position="bottom">
             <RetainLink
               className={buttonVariants.outlineIcon}
               href="/classes/add"
             >
-              <HiOutlinePlusCircle /> Add
+              <HiOutlinePlusCircle />
             </RetainLink>
           </Tooltip>
         </div>
       </div>
       {children}
-    </SidebarLayout>
+    </aside>
   );
 }
--- src/components/DragOverlay/EventDragOverlay.tsx
+++ src/components/DragOverlay/EventDragOverlay.tsx
@@ -6,7 +6,7 @@ import type { Preference } from "~/lib/definitions";
 import { getDayEnum, getRowIndex } from "~/lib/functions";
 
 export default function EventDragOverlay() {
-  const { events, activeCourse } = usePreview();
+  const { events, activeCourse, over } = usePreview();
   const event: Preference = events.find(
     (course) =>
       course.title === activeCourse?.title &&
@@ -19,15 +19,15 @@ export default function EventDragOverlay() {
   }
   const rowSpan: number = event.time.duration / 30;
   const colourVariants = {
-    0: "bg-purple-400/40 text-purple-800 shadow-purple-100 border-l-[7px] border-l-purple-500 dark:text-purple-400 dark:bg-purple-700/40 dark:border-l-purple-800",
-    1: "bg-yellow-400/40 text-yellow-800 shadow-yellow-100 border-l-[7px] border-l-yellow-500 dark:text-yellow-400 dark:bg-yellow-700/40 dark:border-l-yellow-800",
-    2: "bg-orange-400/40 text-orange-800 shadow-orange-100 border-l-[7px] border-l-orange-500 dark:text-orange-400 dark:bg-orange-700/40 dark:border-l-orange-800",
-    3: "bg-red-400/40 text-red-800 shadow-red-100 border-l-[7px] border-l-red-500 dark:text-red-400 dark:bg-red-700/40 dark:border-l-red-800",
-    4: "bg-green-400/40 text-green-800 shadow-green-100 border-l-[7px] border-l-green-500 dark:text-green-400 dark:bg-green-700/40 dark:border-l-green-800",
-    5: "bg-teal-400/40 text-teal-800 shadow-teal-100 border-l-[7px] border-l-teal-500 dark:text-teal-400 dark:bg-teal-700/40 dark:border-l-teal-800",
-    6: "bg-blue-400/40 text-blue-800 shadow-blue-100 border-l-[7px] border-l-blue-500 dark:text-blue-400 dark:bg-blue-700/40 dark:border-l-blue-800",
-    7: "bg-fuchsia-400/40 text-fuchsia-800 shadow-fuchsia-100 border-l-[7px] border-l-fuchsia-500 dark:text-fuchsia-400 dark:bg-fuchsia-800/40 dark:border-l-fuchsia-800",
-    8: "bg-pink-400/40 text-pink-800 shadow-pink-100 border-l-[7px] border-l-pink-500 dark:text-pink-400 dark:bg-pink-700/40 dark:border-l-pink-800",
+    0: "bg-purple-400/40 text-purple-800 shadow-purple-100 border-l-[7px] border-l-purple-500",
+    1: "bg-yellow-400/40 text-yellow-800 shadow-yellow-100 border-l-[7px] border-l-yellow-500",
+    2: "bg-orange-400/40 text-orange-800 shadow-orange-100 border-l-[7px] border-l-orange-500",
+    3: "bg-red-400/40 text-red-800 shadow-red-100 border-l-[7px] border-l-red-500",
+    4: "bg-green-400/40 text-green-800 shadow-green-100 border-l-[7px] border-l-green-500",
+    5: "bg-teal-400/40 text-teal-800 shadow-teal-100 border-l-[7px] border-l-teal-500",
+    6: "bg-blue-400/40 text-blue-800 shadow-blue-100 border-l-[7px] border-l-blue-500",
+    7: "bg-fuchsia-400/40 text-fuchsia-800 shadow-fuchsia-100 border-l-[7px] border-l-fuchsia-500",
+    8: "bg-pink-400/40 text-pink-800 shadow-pink-100 border-l-[7px] border-l-pink-500",
   };
   return (
     <DragOverlay
@@ -38,7 +38,9 @@ export default function EventDragOverlay() {
       }}
       className={`m-0.5 flex flex-col overflow-hidden ${
         colourVariants[event.colour]
-      } rounded px-3 py-2 `}
+      } rounded px-3 py-2 ${
+        over ? "hover:cursor-copy" : "hover:cursor-grabbing"
+      }`}
     >
       {activeCourse && (
         <Event title={event.title} type={event.type} time={event.time} />
