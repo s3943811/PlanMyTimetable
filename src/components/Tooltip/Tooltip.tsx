91c244d41108146dbe14daae04f8851ec1ba5080
s3943811 <s3943811@student.rmit.edu.au>
2023-12-09 22:38:42 +1100

    Refactor Tooltip component to use extend div element and use cn func

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,12 +1,18 @@
+import { cn } from "~/lib/utils";
+
+interface ToolTipProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+  message: React.ReactNode;
+  children: React.ReactNode;
+  position: "top" | "left" | "right" | "bottom";
+}
+
 export default function Tooltip({
   message,
   children,
   position,
-}: {
-  message: React.ReactNode;
-  children: React.ReactNode;
-  position: "top" | "left" | "right" | "bottom";
-}) {
+  className,
+  ...props
+}: ToolTipProps) {
   const positionClass = {
     bottom: "top-10",
     top: "bottom-10",
@@ -14,10 +20,14 @@ export default function Tooltip({
     right: "left-10",
   };
   return (
-    <div className="group relative flex">
+    <div className="group relative flex" {...props}>
       {children}
       <span
-        className={`absolute ${positionClass[position]} z-[999] scale-0 whitespace-nowrap rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 dark:bg-neutral-200 dark:text-neutral-950`}
+        className={cn(
+          `absolute z-[999] scale-0 whitespace-nowrap rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 dark:bg-neutral-200 dark:text-neutral-950`,
+          positionClass[position],
+          className,
+        )}
       >
         {message}
       </span>
