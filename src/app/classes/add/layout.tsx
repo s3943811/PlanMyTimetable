edbc33407cac67a2975c699d4519cebf5f4dc3c7
s3943811 <s3943811@student.rmit.edu.au>
2023-12-09 23:21:32 +1100

    Update class add layout to include block out times in calendar

--- src/app/classes/add/layout.tsx
+++ src/app/classes/add/layout.tsx
@@ -4,7 +4,8 @@ export default function AddLayout({ children }: { children: React.ReactNode }) {
     <section className="container flex grow flex-col p-8 dark:bg-neutral-900 dark:text-white">
       <h2 className="text-xl font-medium tracking-tight">Add classes</h2>
       <p className=" text-neutral-500 dark:text-neutral-400">
-        Add class details, times, and locations.
+        Add class details, times, locations and block out times in your
+        calendar.
       </p>
       <PageSelector />
       <div
