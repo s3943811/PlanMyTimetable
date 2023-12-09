e563eb6851384d4047227eafdcfbe34eba7f9b98
s3943811 <s3943811@student.rmit.edu.au>
2023-12-09 23:08:36 +1100

    Add PageSelector component and remove old hard-coded page selector code

--- src/app/classes/add/layout.tsx
+++ src/app/classes/add/layout.tsx
@@ -1,3 +1,4 @@
+import PageSelector from "./PageSelector";
 export default function AddLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="container flex grow flex-col p-8 dark:bg-neutral-900 dark:text-white">
@@ -5,6 +6,12 @@ export default function AddLayout({ children }: { children: React.ReactNode }) {
       <p className=" text-neutral-500 dark:text-neutral-400">
         Add class details, times, and locations.
       </p>
+      <PageSelector />
+      <div
+        data-orientation="horizontal"
+        role="none"
+        className=" my-3 h-[1px] w-full shrink-0 bg-neutral-200 dark:bg-neutral-600"
+      ></div>
       {children}
     </section>
   );
