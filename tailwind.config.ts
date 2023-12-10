91f930fae3054189d45bef015aafb57d8de80f0f
s3943811 <s3943811@student.rmit.edu.au>
2023-12-11 00:11:13 +1100

    Add @radix-ui/react-accordion package and update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,20 @@ export default {
   content: ["./src/**/*.{tsx,ts}"],
   theme: {
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       gridTemplateRows: {
         "48": "repeat(48, minmax(0, 1fr))",
       },
