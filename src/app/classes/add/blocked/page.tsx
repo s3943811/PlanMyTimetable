15f56863099d1d00ef0c3910c8b971f4b02402ca
s3943811 <s3943811@student.rmit.edu.au>
2023-12-10 16:53:02 +1100

    Refactor blocked page to include additional information

--- src/app/classes/add/blocked/page.tsx
+++ src/app/classes/add/blocked/page.tsx
@@ -1,5 +1,13 @@
 import BlockedForm from "./BlockedForm";
 
 export default function Page() {
-  return <BlockedForm />;
+  return (
+    <>
+      <p className=" text-neutral-700 dark:text-neutral-200">
+        Please note blocked times hide options for being displayed if they are
+        in that blocked time.
+      </p>
+      <BlockedForm />
+    </>
+  );
 }
