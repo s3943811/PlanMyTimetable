cb7147379769d66fe83208988f7cc05eecd2526e
s3943811 <s3943811@student.rmit.edu.au>
2024-01-21 16:42:06 +1100

    Refactor EventBody to have conditionally render the event content based on the size of the event (duration) and update styling of EventBody

--- src/components/Event/EventBody.tsx
+++ src/components/Event/EventBody.tsx
@@ -2,6 +2,7 @@ import { CourseType } from "~/lib/definitions";
 import type { Time } from "~/lib/definitions";
 import { Duration, DateTime } from "luxon";
 import { useMemo } from "react";
+import { cn } from "~/lib/utils";
 
 interface EventProps {
   title: string;
@@ -15,20 +16,46 @@ export default function Event({ title, type, time }: EventProps) {
     return endTime.plus(duration);
   }, [time]);
 
+  /**
+   * There are 3 return scenarios here depending on how large (the duration) the div will be,
+   * so either return just the Title of the class - 30 minutes
+   * the title, type and times - 60 minutes
+   * the title, type, times and location - anything above 60 minutes
+   */
   return (
     <>
-      <h1 className="truncate text-sm font-medium tracking-tight xl:text-base">
+      <h1 className="line-clamp-3 text-sm font-medium lg:line-clamp-none xl:text-base">
         {title}
       </h1>
-      <p className=" whitespace-pre-wrap text-xs font-normal tracking-tighter sm:whitespace-nowrap md:text-sm lg:whitespace-normal">
-        {time.start} - {endTime.toFormat("HH:mm")}
-      </p>
-      <p className=" text-xs ">
-        {CourseType[type]}
-        <span className="hidden md:contents md:before:content-[',_']">
-          {time.campus_description}&nbsp;({time.location})
-        </span>
-      </p>
+      {time.duration > 30 && (
+        <>
+          <p
+            className={cn(
+              `text-xs 2xl:text-sm`,
+              time.duration > 60 ? "" : "hidden sm:block",
+            )}
+          >
+            {CourseType[type]}
+          </p>
+          <p
+            className={cn(
+              "whitespace-pre-line text-xs 2xl:text-sm",
+              time.duration > 60 ? "" : "hidden sm:block",
+            )}
+          >
+            {time.start} <span className="hidden md:contents">-</span>{" "}
+            {endTime.toFormat("HH:mm")}
+          </p>
+        </>
+      )}
+
+      {time.duration > 60 && (
+        <>
+          <p className="line-clamp-2 hidden text-xs lg:block">
+            {time.campus_description}&nbsp;({time.location})
+          </p>
+        </>
+      )}
     </>
   );
 }
